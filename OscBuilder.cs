99d9fbff82c7b2edb1b07158fcb4afd615fb20ae
Michael <michael-sargent@hotmail.com>
2015-10-27 18:49:52 -0700

    added OSC message builder class

---
+++ OscBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using Rug.Osc;
+
+namespace LaptopOrchestra.Kinect
+{
+    class OscBuilder
+    {
+        public OscMessage BuildJointMessage(Joint joint)
+        {	
+			var address = String.Format("/kinect/joint/{0}", joint.JointType);
+			var pos = joint.Position;
+			return new OscMessage(address, pos.X, pos.Y, pos.Z);
+        }
+
+        public OscMessage BuildVectorMessage(Joint joint1, Joint joint2)
+        {	
+			var address = String.Format("/kinect/vector/{0}/{1}", joint1.JointType, joint2.JointType);
+			var xDist = joint2.Position.X - joint1.Position.X;
+            var yDist = joint2.Position.Y - joint1.Position.Y;
+            var zDist = joint2.Position.Z - joint1.Position.Z;
+			return new OscMessage(address, xDist , yDist , zDist );
+        }
+
+        public OscMessage BuildDistanceMessage(Joint joint1, Joint joint2)
+        {	
+			var address = String.Format("/kinect/vector/{0}/{1}", joint1.JointType, joint2.JointType);
+			var xDist = joint2.Position.X - joint1.Position.X;
+            var yDist = joint2.Position.Y - joint1.Position.Y;
+            var zDist = joint2.Position.Z - joint1.Position.Z;
+            var absDist = Math.Sqrt(xDist * xDist + yDist * yDist* + zDist * zDist);
+			return new OscMessage(address, absDist);
+        }
+
+        //incomplete
+        public OscMessage BuildHandMessage(HandState hand)
+        {
+            var address = String.Format("/kinect/hand/");
+            return new OscMessage(address);
+        }
+    }
+}
